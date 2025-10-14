727ce9d4fc53044ddf2c239c8b6578de5ed1e343
naginenihari <naginenihari@gmail.com>
2025-10-14 21:32:13 +0530

    count based loop

---
+++ Count/variables.tf
@@ -0,0 +1,11 @@
+variable "instances" {
+  default = ["mongodb","catalogue","redis","user"]
+}
+
+variable "zone_id" {
+  default = "Z05202297D7QR4S9L2JB"
+}
+
+variable "domain_name" {
+  default ="naginenihariaws.store"
+}
