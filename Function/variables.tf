949d770fd77ed85da21468fa30d8ba02307aed73
naginenihari <naginenihari@gmail.com>
2025-10-15 18:18:48 +0530

    function created

---
+++ Function/variables.tf
@@ -0,0 +1,11 @@
+variable "instances" {
+  default = ["mongodb","catalogue","redis","user","cart","mysql","shipping","rabbitmq","payment","frontend"]
+}
+
+variable "zone_id" {
+  default = "Z05202297D7QR4S9L2JB"
+}
+
+variable "domain_name" {
+  default ="naginenihariaws.store"
+}
