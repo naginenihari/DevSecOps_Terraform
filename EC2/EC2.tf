a9cf6ebc8681089dab293fecca3d7ab42ac87537
naginenihari <naginenihari@gmail.com>
2025-10-20 21:01:52 +0530

    sg updated

--- EC2/EC2.tf
+++ EC2/EC2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "terraform" {
     ami = "ami-09c813fb71547fc4f"
     instance_type = "t2.micro"
-    vpc_security_group_ids = [aws_security_group.allow_all.id]
+    vpc_security_group_ids = [aws_security_group.allow_all_SG.id]
     tags = {
         Name = "terraform"
         Terraform = "true"
